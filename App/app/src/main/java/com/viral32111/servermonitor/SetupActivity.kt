d74ac51fbc9a3e83ca16d72ed6095581d9bbdcc5
viral32111 <19510403+viral32111@users.noreply.github.com>
2022-12-08 14:17:46 +0000

    Finish setup activity
    Fixed the shadows below the action bar, and added the options menu.
    
    Next step is dark theme and CI workflow to build app.

--- App/app/src/main/java/com/viral32111/servermonitor/SetupActivity.kt
+++ App/app/src/main/java/com/viral32111/servermonitor/SetupActivity.kt
@@ -2,12 +2,50 @@ package com.viral32111.servermonitor
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.widget.Toolbar
+import androidx.appcompat.app.ActionBar
 
 class SetupActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_setup)
 
-        actionBar?.title = "Setup"
-    }
+	override fun onCreate( savedInstanceState: Bundle? ) {
+		super.onCreate( savedInstanceState )
+		setContentView( R.layout.activity_setup )
+
+		/* ----------------------------- */
+
+		/*actionBar?.title = "Hello World 1"
+		actionBar?.subtitle = "Hello World 2"
+
+		actionBar?.setDisplayShowTitleEnabled( true )
+		actionBar?.setDisplayShowHomeEnabled( true )
+		actionBar?.setDisplayShowCustomEnabled( true )
+
+		actionBar?.elevation = 12.0f
+
+		actionBar?.show()*/
+
+		/* ----------------------------- */
+
+		supportActionBar?.displayOptions = ActionBar.DISPLAY_SHOW_CUSTOM
+		supportActionBar?.setCustomView( R.layout.action_bar );
+
+
+		//supportActionBar?.title = getString( R.string.setup_actionbar_title )
+		//supportActionBar?.subtitle = "Hello World 3"
+
+		//supportActionBar?.setDisplayShowTitleEnabled( true )
+		//supportActionBar?.setDisplayShowHomeEnabled( true )
+		//supportActionBar?.setDisplayShowCustomEnabled( true )
+
+		//supportActionBar?.elevation = 8.0f
+
+	}
+
+	/*override fun onCreateOptionsMenu( menu: Menu? ): Boolean {
+		//return super.onCreateOptionsMenu( menu )
+		menuInflater.inflate( R.menu.actionbar_menu, menu )
+		return true
+	}*/
+
 }
