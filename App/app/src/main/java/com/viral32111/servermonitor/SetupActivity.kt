05e8ca54dc49c883b2c7a0abea51c8b02791f1dd
viral32111 <19510403+viral32111@users.noreply.github.com>
2022-12-10 14:46:11 +0000

    Simple UI interaction with popup messages

--- App/app/src/main/java/com/viral32111/servermonitor/SetupActivity.kt
+++ App/app/src/main/java/com/viral32111/servermonitor/SetupActivity.kt
@@ -1,51 +1,89 @@
 package com.viral32111.servermonitor
 
+import android.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import android.widget.Toolbar
 import androidx.appcompat.app.ActionBar
+import com.google.android.material.snackbar.Snackbar
 
 class SetupActivity : AppCompatActivity() {
 
+	// Runs when the activity is created...
 	override fun onCreate( savedInstanceState: Bundle? ) {
+
+		// Run default action & display the relevant layout file
 		super.onCreate( savedInstanceState )
 		setContentView( R.layout.activity_setup )
 
-		/* ----------------------------- */
-
-		/*actionBar?.title = "Hello World 1"
-		actionBar?.subtitle = "Hello World 2"
-
-		actionBar?.setDisplayShowTitleEnabled( true )
-		actionBar?.setDisplayShowHomeEnabled( true )
-		actionBar?.setDisplayShowCustomEnabled( true )
-
-		actionBar?.elevation = 12.0f
-
-		actionBar?.show()*/
-
-		/* ----------------------------- */
-
+		// Switch to the custom Material Toolbar
 		supportActionBar?.displayOptions = ActionBar.DISPLAY_SHOW_CUSTOM
-		supportActionBar?.setCustomView( R.layout.action_bar );
+		supportActionBar?.setCustomView( R.layout.action_bar )
 
+		// Get all UI controls
+		val editTextInstanceUrl = findViewById<EditText>( R.id.setupUrlEditText )
+		val editTextAuthenticationUsername = findViewById<EditText>( R.id.setupAuthenticationUsernameEditText )
+		val editTextAuthenticationPassword = findViewById<EditText>( R.id.setupAuthenticationPasswordEditText )
+		val buttonContinue = findViewById<Button>( R.id.setupContinueButton )
 
-		//supportActionBar?.title = getString( R.string.setup_actionbar_title )
-		//supportActionBar?.subtitle = "Hello World 3"
+		// Register event handler for when the the continue button is pressed...
+		buttonContinue.setOnClickListener {
 
-		//supportActionBar?.setDisplayShowTitleEnabled( true )
-		//supportActionBar?.setDisplayShowHomeEnabled( true )
-		//supportActionBar?.setDisplayShowCustomEnabled( true )
+			// Get the values in the text inputs
+			val instanceUrl = editTextInstanceUrl.text
+			val authUsername = editTextAuthenticationUsername.text
+			val authPassword = editTextAuthenticationPassword.text
 
-		//supportActionBar?.elevation = 8.0f
+			// Do not continue if an instance URL wasn't provided
+			if ( instanceUrl.isEmpty() ) {
+				showBriefMessage( R.string.setupToastInstanceUrlEmpty )
+				return@setOnClickListener
+			}
+
+			// Do not continue if a username wasn't provided
+			if ( authUsername.isEmpty() ) {
+				showBriefMessage( R.string.setupToastAuthenticationUsernameEmpty )
+				return@setOnClickListener
+			}
+
+			// Do not continue if a password wasn't provided
+			if ( authPassword.isEmpty() ) {
+				showBriefMessage( R.string.setupToastAuthenticationPasswordEmpty )
+				return@setOnClickListener
+			}
+
+			/*val alertDialogBuilder = AlertDialog.Builder( this )
+			alertDialogBuilder.setTitle( "Error" )
+			alertDialogBuilder.setMessage( "The URL to an instance must be provided." )
+			alertDialogBuilder.setNeutralButton( "OK" ) { _, _ ->
+				Toast.makeText( this, "neutral", Toast.LENGTH_SHORT ).show()
+			}
+			alertDialogBuilder.setOnCancelListener {
+				Toast.makeText( this, "cancelled", Toast.LENGTH_SHORT ).show()
+			}
+			alertDialogBuilder.setOnDismissListener {
+				Toast.makeText( this, "dismissed", Toast.LENGTH_SHORT ).show()
+			}
+			val alertDialog = alertDialogBuilder.create()
+			alertDialog.show()*/
+
+			showBriefMessage( "Everything is good!" )
+
+		}
 
 	}
 
-	/*override fun onCreateOptionsMenu( menu: Menu? ): Boolean {
-		//return super.onCreateOptionsMenu( menu )
-		menuInflater.inflate( R.menu.actionbar_menu, menu )
-		return true
-	}*/
+	private fun showBriefMessage( stringId: Int ) {
+		Toast.makeText( this, getString( stringId ), Toast.LENGTH_SHORT ).show()
+	}
+
+	private fun showBriefMessage( message: String ) {
+		Toast.makeText( this, message, Toast.LENGTH_SHORT ).show()
+	}
 
 }
