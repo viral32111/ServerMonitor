c84e97e3e183c4f4a397b2e7b80214985dbd70e4
viral32111 <19510403+viral32111@users.noreply.github.com>
2022-12-17 17:09:33 +0000

    Setup for settings activity

---
+++ App/app/src/main/java/com/viral32111/servermonitor/SettingsActivity.kt
@@ -0,0 +1,13 @@
+package com.viral32111.servermonitor
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SettingsActivity : AppCompatActivity() {
+
+	override fun onCreate( savedInstanceState: Bundle? ) {
+		super.onCreate( savedInstanceState )
+		setContentView( R.layout.activity_settings )
+	}
+
+}
