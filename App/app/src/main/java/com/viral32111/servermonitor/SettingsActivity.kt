f502e3d6e04cce63bd63bf6e1719a01be1c6fbfc
viral32111 <19510403+viral32111@users.noreply.github.com>
2022-12-18 17:40:55 +0000

    Progress on Settings & Setup activity
    Added animations between activity switching, made the toolbar a bit more universal, refined code, added notes for future work

--- App/app/src/main/java/com/viral32111/servermonitor/SettingsActivity.kt
+++ App/app/src/main/java/com/viral32111/servermonitor/SettingsActivity.kt
@@ -2,12 +2,59 @@ package com.viral32111.servermonitor
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import androidx.appcompat.app.ActionBar
+import androidx.appcompat.content.res.AppCompatResources
+import com.google.android.material.appbar.MaterialToolbar
 
 class SettingsActivity : AppCompatActivity() {
 
+	// Runs when the activity is created...
 	override fun onCreate( savedInstanceState: Bundle? ) {
+
+		// Run default action & display the relevant layout file
 		super.onCreate( savedInstanceState )
 		setContentView( R.layout.activity_settings )
+
+		// Switch to the custom Material Toolbar
+		supportActionBar?.displayOptions = ActionBar.DISPLAY_SHOW_CUSTOM
+		supportActionBar?.setCustomView( R.layout.action_bar )
+
+		// Get all the UI controls
+		val materialToolbar = supportActionBar?.customView?.findViewById<MaterialToolbar>( R.id.actionBarMaterialToolbar )
+		val instanceUrlEditText = findViewById<EditText>( R.id.settingsInstanceUrlEditText )
+		val credentialsUsernameEditText = findViewById<EditText>( R.id.settingsCredentialsUsernameEditText )
+		val credentialsPasswordEditText = findViewById<EditText>( R.id.settingsCredentialsPasswordEditText )
+		val saveButton = findViewById<Button>( R.id.settingsSaveButton )
+
+		// Set the title on the toolbar
+		materialToolbar?.title = getString( R.string.settingsActionBarTitle )
+		materialToolbar?.isTitleCentered = true
+
+		// Enable the back button on the toolbar
+		materialToolbar?.navigationIcon = AppCompatResources.getDrawable( this, R.drawable.ic_baseline_arrow_back_24 )
+		materialToolbar?.setNavigationOnClickListener {
+			finish()
+			overridePendingTransition( R.anim.slide_in_from_left, R.anim.slide_out_to_right )
+		}
+
+		// Disable the menu on the toolbar
+		materialToolbar?.menu?.clear()
+
+		// When the the save button is pressed...
+		saveButton.setOnClickListener {
+
+			// TODO: Get all values from UI inputs
+			// TODO: Validate all those values
+			// TODO: Save those values to shared preferences
+
+			// Return to last activity
+			finish()
+			overridePendingTransition( R.anim.slide_in_from_left, R.anim.slide_out_to_right )
+
+		}
+
 	}
 
 }
--- App/app/src/main/java/com/viral32111/servermonitor/SetupActivity.kt
+++ App/app/src/main/java/com/viral32111/servermonitor/SetupActivity.kt
@@ -1,10 +1,12 @@
 package com.viral32111.servermonitor
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import androidx.appcompat.app.ActionBar
+import com.google.android.material.appbar.MaterialToolbar
 import com.google.android.material.snackbar.Snackbar
 
 class SetupActivity : AppCompatActivity() {
@@ -21,12 +23,17 @@ class SetupActivity : AppCompatActivity() {
 		supportActionBar?.setCustomView( R.layout.action_bar )
 
 		// Get all UI controls
-		val editTextInstanceUrl = findViewById<EditText>( R.id.setupUrlEditText )
+		val materialToolbar = supportActionBar?.customView?.findViewById<MaterialToolbar>( R.id.actionBarMaterialToolbar )
+		val editTextInstanceUrl = findViewById<EditText>( R.id.settingsInstanceUrlEditText )
 		val editTextAuthenticationUsername = findViewById<EditText>( R.id.setupAuthenticationUsernameEditText )
 		val editTextAuthenticationPassword = findViewById<EditText>( R.id.setupAuthenticationPasswordEditText )
-		val buttonContinue = findViewById<Button>( R.id.setupContinueButton )
+		val buttonContinue = findViewById<Button>( R.id.settingsSaveButton )
 
-		// Register event handler for when the the continue button is pressed...
+		// Set the title on the toolbar
+		materialToolbar?.title = getString( R.string.setup_action_bar_title )
+		materialToolbar?.isTitleCentered = true
+
+		// When the the continue button is pressed...
 		buttonContinue.setOnClickListener {
 
 			// Get the values in the text inputs
@@ -52,18 +59,42 @@ class SetupActivity : AppCompatActivity() {
 				return@setOnClickListener
 			}
 
+			// TODO: Check if URL is valid
+			// TODO: Check if username is valid (length & character requirements)
+			// TODO: Check if password is valid (strength, length & character requirements)
+
+			// TODO: Attempt connection to URL and validate the connection point service is running on it
+
+			// TODO: Save values to shared preferences
+
 			// For debugging!
 			showBriefMessage( "Everything is good!" )
+
+		}
+
+		// When a menu item on the action bar is pressed...
+		materialToolbar?.setOnMenuItemClickListener { menuItem ->
+
+			// Settings
+			if ( menuItem.title?.equals( getString( R.string.action_bar_menu_settings ) ) == true ) {
+				startActivity( Intent( this, SettingsActivity::class.java ) )
+				overridePendingTransition( R.anim.slide_in_from_right, R.anim.slide_out_to_left )
+			}
+
+			// Always successful
+			return@setOnMenuItemClickListener true
 
 		}
 
 	}
 
 	// https://developer.android.com/develop/ui/views/notifications/snackbar/showing
+	// TODO: Move this to a shared class
 	private fun showBriefMessage( stringId: Int ) {
 		Snackbar.make( findViewById( R.id.setupConstraintLayout ), stringId, Snackbar.LENGTH_SHORT ).show()
 	}
 
+	// TODO: This shouldn't be used in the final build, always use string IDs
 	private fun showBriefMessage( message: String ) {
 		Snackbar.make( findViewById( R.id.setupConstraintLayout ), message, Snackbar.LENGTH_SHORT ).show()
 	}
