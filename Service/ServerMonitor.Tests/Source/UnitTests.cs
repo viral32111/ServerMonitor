aa5799f1e26694177be49a1e44cd0e25e0d62555
viral32111 <19510403+viral32111@users.noreply.github.com>
2023-01-23 15:32:06 +0000

    Setup server-side console project and unit testing project

---
+++ Service/ServerMonitor.Tests/Source/UnitTests.cs
@@ -0,0 +1,10 @@
+using System;
+using Xunit;
+
+namespace ServerMonitor.Tests {
+	public class UnitTests {
+		public void ExampleTest() {
+			Assert.True( true );
+		}
+	}
+}
---
+++ Service/ServerMonitor/Source/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace ServerMonitor {
+	public class Program {
+		public async Task Main( string[] arguments ) {
+			Console.WriteLine( "Hello World!" );
+		}
+	}
+}
