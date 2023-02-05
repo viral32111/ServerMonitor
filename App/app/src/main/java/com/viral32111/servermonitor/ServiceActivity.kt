a1dad8a69daf9610e722ee41c923d2dc1c81a786
viral32111 <19510403+viral32111@users.noreply.github.com>
2023-02-05 17:12:05 +0000

    Foundation for Service activity

---
+++ App/app/src/main/java/com/viral32111/servermonitor/ServiceActivity.kt
@@ -0,0 +1,47 @@
+package com.viral32111.servermonitor
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.ActionBar
+import com.google.android.material.appbar.MaterialToolbar
+
+class ServiceActivity : AppCompatActivity() {
+
+	// Runs when the activity is created...
+	override fun onCreate( savedInstanceState: Bundle? ) {
+
+		// Run default action & display the relevant layout file
+		super.onCreate( savedInstanceState )
+		setContentView( R.layout.activity_service )
+		Log.d( Shared.logTag, "Creating activity..." )
+
+		// Switch to the custom Material Toolbar
+		supportActionBar?.displayOptions = ActionBar.DISPLAY_SHOW_CUSTOM
+		supportActionBar?.setCustomView( R.layout.action_bar )
+		Log.d( Shared.logTag, "Switched to Material Toolbar" )
+
+		// Get all UI controls
+		val materialToolbar = supportActionBar?.customView?.findViewById<MaterialToolbar>( R.id.actionBarMaterialToolbar )
+		Log.d( Shared.logTag, "Got UI controls" )
+
+		// Set the title on the toolbar
+		materialToolbar?.title = getString( R.string.serviceActionBarTitle )
+		materialToolbar?.isTitleCentered = true
+		Log.d( Shared.logTag, "Set Material Toolbar title to '${ materialToolbar?.title }' (${ materialToolbar?.isTitleCentered } )" )
+
+		// Open settings when its action bar menu item is clicked
+		materialToolbar?.setOnMenuItemClickListener { menuItem ->
+			if ( menuItem.title?.equals( getString( R.string.action_bar_menu_settings ) ) == true ) {
+				Log.i( Shared.logTag, "Opening Settings activity..." )
+				startActivity( Intent( this, SettingsActivity::class.java ) )
+				overridePendingTransition( R.anim.slide_in_from_right, R.anim.slide_out_to_left )
+			}
+
+			return@setOnMenuItemClickListener true
+		}
+
+	}
+
+}
