58aab05c5872d72b927b6aeb6b970defa6cec18c
viral32111 <19510403+viral32111@users.noreply.github.com>
2023-02-06 17:09:21 +0000

    Progress for dark theme & dynamic material 3 color

---
+++ App/app/src/main/java/com/viral32111/servermonitor/ServerMonitorApplication.kt
@@ -0,0 +1,11 @@
+package com.viral32111.servermonitor
+
+import android.app.Application
+import com.google.android.material.color.DynamicColors
+
+class ServerMonitorApplication : Application() {
+	override fun onCreate() {
+		super.onCreate()
+		DynamicColors.applyToActivitiesIfAvailable( this )
+	}
+}
