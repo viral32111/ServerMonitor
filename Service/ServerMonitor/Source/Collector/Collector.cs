ea12ddadf53ba439934afe833649ece4091b3f8a
viral32111 <19510403+viral32111@users.noreply.github.com>
2023-02-08 13:28:47 +0000

    Collect memory metrics on Windows
    Organised the memory metrics code for Linux too.

--- Service/ServerMonitor/Source/Collector/Collector.cs
+++ Service/ServerMonitor/Source/Collector/Collector.cs
@@ -9,10 +9,7 @@ using System.Collections.Generic;
 
 using Microsoft.Extensions.Logging; // https://learn.microsoft.com/en-us/dotnet/core/extensions/console-log-formatter
 
-/*using System.Diagnostics;
-using System.Diagnostics.PerformanceData;
-using System.Resources;
-using System.Reflection;*/
+using ServerMonitor.Collector.Resource;
 
 namespace ServerMonitor.Collector {
 
@@ -27,96 +24,18 @@ namespace ServerMonitor.Collector {
 			Config configuration = Configuration.Load( extraConfigurationFilePath );
 			logger.LogInformation( "Loaded configuration. Test = {0}", configuration.Test );
 
-			// https://stackoverflow.com/a/278088
-			/*PerformanceCounter processorCounter = new( "Processor", "% Processor Time", "_Total" );
-			PerformanceCounter memoryCounter = new( "Memory", "Available MBytes" );
-
-			logger.LogDebug( "Measuring processor usage, this will take a second..." );
-			processorCounter.NextValue();
-			Thread.Sleep( 1000 );
-			logger.LogInformation( "Processor: {0} %", processorCounter.NextValue() );
-
-			logger.LogInformation( "Memory: {0} MB", memoryCounter.NextValue() );*/
-
+			Memory memory = new();
 			while ( true ) {
-				if ( RuntimeInformation.IsOSPlatform( OSPlatform.Windows ) ) {
-					logger.LogCritical( "No memory usage detection on Windows yet" );
-					break;
-				} else if ( RuntimeInformation.IsOSPlatform( OSPlatform.Linux ) ) {
-					GetSystemResourceUsageLinux();
-				}
+				memory.Update();
+
+				double totalMemory = Math.Round( memory.TotalBytes / 1024 / 1024, 2 );
+				double freeMemory = Math.Round( memory.FreeBytes / 1024 / 1024, 2 );
+				double usedMemory = Math.Round( memory.GetUsedBytes() / 1024 / 1024, 2 );
+				double usedMemoryPercentage = Math.Round( memory.GetUsedPercentage() );
+				logger.LogInformation( "Memory: {0} MiB / {1} MiB ({2} MiB free, {2}% usage)", usedMemory, totalMemory, freeMemory, usedMemoryPercentage );
 
 				Thread.Sleep( 1000 );
 			}
-		}
-
-		private static void GetSystemResourceUsageLinux() {
-
-			// Read /proc/meminfo
-			using ( FileStream fileStream = new( "/proc/meminfo", FileMode.Open, FileAccess.Read ) ) {
-				using ( StreamReader streamReader = new( fileStream ) ) {
-					Dictionary<string, float> memoryInformation = new();
-
-					do {
-						string? fileLine = streamReader.ReadLine();
-						if ( fileLine == null ) break;
-
-						string[] lineParts = fileLine.Split( ":", 2 );
-						if ( lineParts.Length != 2 ) throw new Exception( "Unexpected number of parts on line of file" );
-
-						string key = lineParts[ 0 ].Trim();
-						string value = lineParts[ 1 ].Trim();
-						//logger.LogDebug( "Line: '{0}', Key: '{1}', Value: '{2}'", fileLine, key, value );
-
-						string[] valueParts = value.Split( " ", 2 );
-						if ( float.TryParse( valueParts[ 0 ].Trim(), out float valueRaw ) != true ) throw new Exception( "Value is not a float" );
-						if ( valueParts.Length == 2 ) {
-							string valueType = valueParts[ 1 ].Trim();
-
-							// https://superuser.com/q/1737654
-							if ( valueType == "kB" ) {
-								float valueInMegabytes = valueRaw / 1024;
-								//int valueInGigabytes = valueInMegabytes / 1024;
-								memoryInformation.Add( key, valueInMegabytes );
-							} else {
-								throw new Exception( "Unexpected value type" );
-							}
-						} else {
-							memoryInformation.Add( key, valueRaw );
-						}
-
-					} while ( !streamReader.EndOfStream );
-
-					// https://stackoverflow.com/a/41251290
-					float totalMemory = memoryInformation[ "MemTotal" ];
-					float freeMemory = memoryInformation[ "MemFree" ];
-					float cachedMemory = memoryInformation[ "Cached" ];// + memoryInformation[ "SReclaimable" ] - memoryInformation[ "Shmem" ];
-					float bufferedMemory = memoryInformation[ "Buffers" ];
-
-					float usedMemory = totalMemory - freeMemory - cachedMemory - bufferedMemory;
-					float usedMemoryPercentage = ( usedMemory / totalMemory ) * 100;
-
-					float totalSwap = memoryInformation[ "SwapTotal" ];
-					float freeSwap = memoryInformation[ "SwapFree" ];
-					float usedSwap = totalSwap - freeSwap;
-					float usedSwapPercentage = ( usedSwap / totalSwap ) * 100;
-
-					logger.LogInformation( "Memory: {0} MiB / {1} MiB ({2} MiB free, {2}% usage)", Math.Round( usedMemory, 2 ), Math.Round( totalMemory, 2 ), Math.Round( freeMemory, 2 ), Math.Round( usedMemoryPercentage, 0 ) );
-					logger.LogInformation( "Swap: {0} MiB / {1} MiB ({2} MiB free, {2}% usage)", Math.Round( usedSwap, 2 ), Math.Round( totalSwap, 2 ), Math.Round( freeSwap, 2 ), Math.Round( usedSwapPercentage, 0 ) );
-
-					//float totalUsedMemory = memoryInformation[ "MemTotal" ] - memoryInformation[ "MemFree" ]; //- memoryInformation[ "Buffers" ] - memoryInformation[ "Cached" ];
-					//float cachedAndBufferedMemory = memoryInformation[ "Buffers" ] + memoryInformation[ "Cached" ]; 
-
-					/*logger.LogInformation( "Total Memory: {0} MiB", memoryInformation[ "MemTotal" ] );
-					logger.LogInformation( "Free Memory: {0} MiB", memoryInformation[ "MemFree" ] );
-					logger.LogInformation( "Available Memory: {0} MiB", memoryInformation[ "MemAvailable" ] );
-					logger.LogInformation( "Buffered Memory: {0} MiB", memoryInformation[ "Buffers" ] );
-					logger.LogInformation( "Cached Memory: {0} MiB", memoryInformation[ "Cached" ] );
-					logger.LogInformation( "Total Swap: {0} MiB", memoryInformation[ "SwapTotal" ] );
-					logger.LogInformation( "Free Swap: {0} MiB", memoryInformation[ "SwapFree" ] );*/
-				}
-			}
-
 		}
 
 	}
--- Service/ServerMonitor/Source/Collector/Resource/Memory.cs
+++ Service/ServerMonitor/Source/Collector/Resource/Memory.cs
@@ -1,38 +1,111 @@
 using System;
+using System.IO;
 using System.Runtime.InteropServices;
+using System.Collections.Generic;
 
-/*
-Memory systemMemory = new();
-systemMemory.Update();
-Console.WriteLine( systemMemory.TotalBytes, systemMemory.FreeBytes, systemMemory.UsedBytes );
-*/
+using Microsoft.Extensions.Logging; // https://learn.microsoft.com/en-us/dotnet/core/extensions/console-log-formatter
 
 namespace ServerMonitor.Collector.Resource {
-	public class Memory {
-		public readonly int TotalBytes;
-		public readonly int FreeBytes;
-		public readonly int UsedBytes;
 
+	// Encapsulates collecting system memory metrics
+	public class Memory {
+
+		// Create the logger for this file
+		private static readonly ILogger logger = Logging.CreateLogger( "Collector/Resource/Memory" );
+
+		// Holds the metrics from the latest update
+		public double TotalBytes { get; private set; }
+		public double FreeBytes { get; private set; }
+
+		// Simply returns the memory utilization
+		public double GetUsedBytes() => TotalBytes - FreeBytes;
+		public double GetUsedPercentage() => ( GetUsedBytes() / TotalBytes ) * 100;
+
+		// Calls the appropriate update function depending on the operating system...
 		public void Update() {
 			if ( RuntimeInformation.IsOSPlatform( OSPlatform.Windows ) ) UpdateOnWindows();
 			else if ( RuntimeInformation.IsOSPlatform( OSPlatform.Linux ) ) UpdateOnLinux();
 			else throw new Exception( "Unsupported operating system" );
 		}
 
+		// Updates the metrics for Windows...
 		private void UpdateOnWindows() {
 			if ( !RuntimeInformation.IsOSPlatform( OSPlatform.Windows ) ) throw new InvalidOperationException( "Method only available on Windows" );
 
-			// TODO: PerformanceCounter...
+			// Call the Windows API function to populate the structure with raw data - https://stackoverflow.com/a/105109
+			MEMORYSTATUSEX memoryStatus = new();
+			if ( !GlobalMemoryStatusEx( memoryStatus ) ) throw new Exception( "Failed to get system memory status" );
 
-			throw new NotImplementedException();
+			// Cast the raw data to the appropriate types & store it in the properties
+			TotalBytes = ( double ) memoryStatus.ullTotalPhys;
+			FreeBytes = ( double ) memoryStatus.ullAvailPhys;
 		}
 
+		// Updates the metrics for Linux...
 		private void UpdateOnLinux() {
 			if ( !RuntimeInformation.IsOSPlatform( OSPlatform.Windows ) ) throw new InvalidOperationException( "Method only available on Linux" );
 
-			// TODO: /proc/meminfo
+			// Read the psuedo-file to get the current memory information
+			using ( FileStream fileStream = new( "/proc/meminfo", FileMode.Open, FileAccess.Read ) ) {
+				using ( StreamReader streamReader = new( fileStream ) ) {
 
-			throw new NotImplementedException();
+					// Create a dictionary to hold the final memory information
+					Dictionary<string, double> memoryInformation = new();
+
+					// Read each line of the file until we reach the end...
+					do {
+						string? fileLine = streamReader.ReadLine();
+						if ( string.IsNullOrWhiteSpace( fileLine ) ) break;
+
+						// Split the line into the name & data (e.g., "MemTotal:       123456 kB")
+						string[] lineParts = fileLine.Split( ":", 2 );
+						if ( lineParts.Length != 2 ) throw new Exception( "Unexpected number of parts on file line" );
+
+						// Remove any whitespace in the name & data
+						string name = lineParts[ 0 ].Trim(), data = lineParts[ 1 ].Trim();
+
+						// Split the data into value & suffix
+						string[] dataParts = data.Split( " ", 2 );
+						if ( double.TryParse( dataParts[ 0 ].Trim(), out double value ) != true ) throw new Exception( "Data value is invalid (cannot be cast to double)" );
+
+						// If there is a suffix, convert the value down to bytes
+						if ( dataParts.Length == 2 ) {
+							string suffix = dataParts[ 1 ].Trim();
+
+							if ( suffix == "kB" ) memoryInformation.Add( name, value * 1024 ); // https://superuser.com/q/1737654
+							else throw new Exception( "Unrecognised suffix" );
+
+						// No suffix so just add the value as-is
+						} else {
+							memoryInformation.Add( name, value );
+						}
+					} while ( !streamReader.EndOfStream );
+
+					TotalBytes = memoryInformation[ "MemTotal" ];
+					FreeBytes = memoryInformation[ "MemFree" ] - memoryInformation[ "Cached" ] - memoryInformation[ "Buffers" ];
+				}
+			}
 		}
+
+		// C++ Windows API structure for GlobalMemoryStatusEx() - https://learn.microsoft.com/en-us/windows/win32/api/sysinfoapi/ns-sysinfoapi-memorystatusex
+		[ StructLayout( LayoutKind.Sequential, CharSet = CharSet.Auto ) ]
+		private class MEMORYSTATUSEX {
+			public uint dwLength = ( uint ) Marshal.SizeOf( typeof( MEMORYSTATUSEX ) );
+			public uint dwMemoryLoad;
+			public ulong ullTotalPhys;
+			public ulong ullAvailPhys;
+			public ulong ullTotalPageFile;
+			public ulong ullAvailPageFile;
+			public ulong ullTotalVirtual;
+			public ulong ullAvailVirtual;
+			public ulong ullAvailExtendedVirtual;
+		}
+
+		// C++ Windows API function to get information about system memory - https://learn.microsoft.com/en-us/windows/win32/api/sysinfoapi/nf-sysinfoapi-globalmemorystatusex
+		// The PerformanceCounter class is not good enough, it does not return enough detail...
+		[ return: MarshalAs( UnmanagedType.Bool ) ]
+		[ DllImport( "kernel32.dll", CharSet = CharSet.Auto, SetLastError = true ) ]
+		private static extern bool GlobalMemoryStatusEx( [ In, Out ] MEMORYSTATUSEX lpBuffer );
+
 	}
 }
