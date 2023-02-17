ad0f0789fe7a0eded9976679bb0ea20b8fc4d7e2
viral32111 <19510403+viral32111@users.noreply.github.com>
2023-02-17 16:07:44 +0000

    Processor frequency on Linux & tidy up Linux code

--- Service/ServerMonitor/Source/Collector/Resource/Processor.cs
+++ Service/ServerMonitor/Source/Collector/Resource/Processor.cs
@@ -1,5 +1,6 @@
 using System;
 using System.IO;
+using System.Linq;
 using System.Threading;
 using System.Management;
 using System.Diagnostics;
@@ -78,64 +79,60 @@ namespace ServerMonitor.Collector.Resource {
 		public override void UpdateOnLinux() {
 			if ( !RuntimeInformation.IsOSPlatform( OSPlatform.Linux ) ) throw new InvalidOperationException( "Method only available on Linux" );
 
-			// Will hold the values for the first iteration
-			int previousTotal = 0;
-			int previousIdle = 0;
-
-			// Loop twice
-			for ( int iteration = 0; iteration < 2; iteration++ ) {
-
-				// Read the psuedo-file to get the current processor statistics
-				using ( FileStream fileStream = new( "/proc/stat", FileMode.Open, FileAccess.Read ) ) {
-					using ( StreamReader streamReader = new( fileStream ) ) {
-
-						// Get the first line of the file
-						string? fileLine = streamReader.ReadLine();
-						if ( string.IsNullOrWhiteSpace( fileLine ) ) throw new Exception( "First line of file is empty or whitespace" );
-
-						// Ensure this is the relevant line
-						if ( !fileLine.StartsWith( "cpu " ) ) throw new Exception( "Unrecognised file line" );
-						fileLine = fileLine.Substring( 4 );
-
-						// Split the line into its components
-						string[] lineParts = fileLine.Split( " ", StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries );
-						if ( lineParts.Length < 4 ) throw new Exception( "File line has incorrect number of parts" );
-
-						// Get the individual times - https://linux.die.net/man/5/proc
-						if ( int.TryParse( lineParts[ 0 ], out int userTime ) != true ) throw new Exception( "Failed to parse user time as integer" );
-						if ( int.TryParse( lineParts[ 1 ], out int niceTime ) != true ) throw new Exception( "Failed to parse nice time as integer" );
-						if ( int.TryParse( lineParts[ 2 ], out int systemTime ) != true ) throw new Exception( "Failed to parse system time as integer" );
-						if ( int.TryParse( lineParts[ 3 ], out int idleTime ) != true ) throw new Exception( "Failed to parse idle time as integer" );
-						int totalTime = userTime + niceTime + systemTime + idleTime;
-
-						// Convert the above times to percentage usage - https://askubuntu.com/a/450136
-						int idleTimeDelta = idleTime - previousIdle;
-						int totalTimeDelta = totalTime - previousTotal;
-						float processorUsage = ( float ) ( 1000 * ( totalTimeDelta - idleTimeDelta ) / ( float ) totalTimeDelta + 5 ) / 10;
-
-						// Set the values for the exported Prometheus metrics
-						Usage.Set( processorUsage );
-						logger.LogDebug( "Updated Prometheus metrics" );
-
-						// Update previous values
-						previousIdle = idleTime;
-						previousTotal = totalTime;
-
-						// Wait before next sample
-						Thread.Sleep( 1000 );
-
-					}
-				}
-
-			}
+			// Get processor usage
+			double processorUsage = GetProcessorUsage();
+			Usage.Set( processorUsage );
 
 			// TODO: Temperature
 			Temperature.Set( 0 );
 
-			// TODO: Frequency
-			Frequency.Set( 0 );
+			// Get processor frequency
+			double processorFrequency = GetProcessorFrequency();
+			Frequency.Set( processorFrequency );
+
+			logger.LogDebug( "Updated Prometheus metrics" );
 
 		}
+
+		// Gets the processor times (for Linux)
+		[ SupportedOSPlatform( "linux" ) ]
+		private int[] GetProcessorTimes() => File.ReadAllLines( "/proc/stat" ) // Read the psuedo-file for processor statistics - https://linux.die.net/man/5/proc
+			.Where( line => line.StartsWith( "cpu " ) ) // Get just the relevant line
+			.Select( line => line.Substring( 4 ) ) // Remove the prefix
+			.Select( line => line.Split( " ", StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries ) ) // Split the line up into values
+			.Select( values => values.Select( part => int.Parse( part ) ).ToArray() ) // Convert the values to integers
+			.First(); // Get the first array
+
+		// Gets the processor usage percentage (for Linux)
+		[ SupportedOSPlatform( "linux" ) ]
+		private double GetProcessorUsage() {
+
+			// Get the processor times
+			int[] firstTimes = GetProcessorTimes();
+			int firstIdleTime = firstTimes[ 3 ];
+			int firstTotalTime = firstTimes.Sum();
+
+			// Wait a second to get a more accurate reading
+			Thread.Sleep( 1000 );
+
+			// Get the processor times again
+			int[] secondTimes = GetProcessorTimes();
+			int secondIdleTime = secondTimes[ 3 ];
+			int secondTotalTime = secondTimes.Sum();
+
+			// Calculate the processor usage from the differences in times - https://askubuntu.com/a/450136
+			int totalTimeDifference = secondTotalTime - firstTotalTime;
+			int idleTimeDifference = secondIdleTime - firstIdleTime;
+			return ( 1000 * ( totalTimeDifference - idleTimeDifference ) / totalTimeDifference + 5 ) / 10.0;
+		}
+
+		// Gets the average processor frequency across all cores in MHz (for Linux)
+		[ SupportedOSPlatform( "linux" ) ]
+		private double GetProcessorFrequency() => File.ReadAllLines( "/proc/cpuinfo" ) // Read the psuedo-file for processor information - https://linux.die.net/man/5/proc
+			.Where( line => !string.IsNullOrWhiteSpace( line ) ) // Skip empty lines
+			.Select( line => line.Split( ":", StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries ) ) // Split the line up into key & value pairs
+			.Where( parts => parts[ 0 ] == "cpu MHz" ) // Skip anything that isn't relevant
+			.Average( parts => double.Parse( parts[ 1 ] ) ); // Calculate the average frequency
 
 	}
 
