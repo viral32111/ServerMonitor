63cca455f0c3497186e8fc661573c2102da884a1
viral32111 <19510403+viral32111@users.noreply.github.com>
2023-02-25 17:36:34 +0000

    Unit test for service metrics

--- Service/ServerMonitor.Tests/Source/MetricsCollectionUnitTests.cs
+++ Service/ServerMonitor.Tests/Source/MetricsCollectionUnitTests.cs
@@ -1,4 +1,3 @@
-using System;
 using Xunit;
 
 namespace ServerMonitor.Tests {
@@ -99,6 +98,25 @@ namespace ServerMonitor.Tests {
 			foreach ( string[] labelValues in network.ReceivedBytes.GetAllLabelValues() ) {
 				string interfaceName = labelValues[ 0 ];
 				Assert.True( network.ReceivedBytes.WithLabels( interfaceName ).Value >= 0, $"Network bytes received is below 0 bytes ({ interfaceName })" );
+			}
+		}
+
+		[ Fact ]
+		public void TestServicesMetrics() {
+			ServerMonitor.Collector.Services services = new( mockConfiguration );
+			services.Update();
+
+			foreach ( string[] labelValues in services.StatusCode.GetAllLabelValues() ) {
+				string service = labelValues[ 0 ];
+				string name = labelValues[ 1 ];
+				string description = labelValues[ 2 ];
+
+				Assert.True( services.StatusCode.WithLabels( service, name, description ).Value >= 0, $"Service status code is below 0 ({ service })" );
+				Assert.True( services.StatusCode.WithLabels( service, name, description ).Value <= 4, $"Service status code is above 4 ({ service })" );
+
+				Assert.True( services.ExitCode.WithLabels( service, name, description ).Value >= 0, $"Service exit code is below 0 ({ service })" );
+
+				Assert.True( services.UptimeSeconds.WithLabels( service, name, description ).Value >= 0, $"Service uptime is below 0 seconds ({ service })" );
 			}
 		}
 
