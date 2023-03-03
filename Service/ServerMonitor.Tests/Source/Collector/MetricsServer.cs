2c759be8060fd259f0a93122a19314cf020b652b
viral32111 <19510403+viral32111@users.noreply.github.com>
2023-03-03 16:42:59 +0000

    Test for Prometheus metrics server

---
+++ Service/ServerMonitor.Tests/Source/Collector/MetricsServer.cs
@@ -0,0 +1,51 @@
+using System.Net.Http;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace ServerMonitor.Tests.Collector {
+
+	public class MetricsServer {
+
+		// Configuration object with all collectors disabled as we're only testing the request/response functionality
+		private static readonly Config mockConfig = new() {
+			PrometheusListenAddress = "127.0.0.1",
+			PrometheusListenPort = 5000,
+			PrometheusListenPath = "metrics/",
+			PrometheusMetricsPrefix = default!,
+
+			CollectProcessorMetrics = false,
+			CollectMemoryMetrics = false,
+			CollectDiskMetrics = false,
+			CollectUptimeMetrics = false,
+			CollectNetworkMetrics = false,
+			CollectFanMetrics = false,
+			CollectPowerMetrics = false,
+			CollectServiceMetrics = false,
+			CollectDockerMetrics = false,
+
+			DockerEngineAPIAddress = default!,
+			DockerEngineAPIVersion = default!
+		};
+
+		[ Fact ]
+		public async Task TestMetricsServer() {
+			// This runs the server in the background, so we can make a request while it's running
+			Task serverTask = Task.Run( () =>
+				ServerMonitor.Collector.Collector.HandleCommand( mockConfig, true )
+			);
+
+			HttpClient httpClient = new();
+			HttpResponseMessage response = await httpClient.GetAsync( string.Format(
+				"http://{0}:{1}/{2}",
+				mockConfig.PrometheusListenAddress,
+				mockConfig.PrometheusListenPort,
+				mockConfig.PrometheusListenPath
+			) );
+			Assert.True( response.IsSuccessStatusCode );
+
+			await serverTask; // Wait for the server to close
+		}
+
+	}
+
+}
