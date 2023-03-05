98b0cce21302eacc85e1d5e4f9abc434f5a573ea
viral32111 <19510403+viral32111@users.noreply.github.com>
2023-03-05 16:33:06 +0000

    Config file for tests, add back defaults, copy configs to build dir

--- Service/ServerMonitor.Tests/Source/Collector/MetricsServer.cs
+++ Service/ServerMonitor.Tests/Source/Collector/MetricsServer.cs
@@ -1,4 +1,3 @@
-using System;
 using System.IO;
 using System.Net.Http;
 using System.Threading.Tasks;
@@ -10,20 +9,8 @@ namespace ServerMonitor.Tests.Collector {
 
 		[ Fact ]
 		public async Task TestMetricsServer() {
-			ServerMonitor.Configuration.Load( Path.Combine( Environment.CurrentDirectory, "config.json" ) );
+			ServerMonitor.Configuration.Load( Path.Combine( Directory.GetCurrentDirectory(), "config.test.json" ) );
 			Assert.NotNull( ServerMonitor.Configuration.Config );
-
-			// Disable all metrics as we're only testing listening functionality
-			ServerMonitor.Configuration.Config.CollectProcessorMetrics = false;
-			ServerMonitor.Configuration.Config.CollectMemoryMetrics = false;
-			ServerMonitor.Configuration.Config.CollectDiskMetrics = false;
-			ServerMonitor.Configuration.Config.CollectNetworkMetrics = false;
-			ServerMonitor.Configuration.Config.CollectUptimeMetrics = false;
-			ServerMonitor.Configuration.Config.CollectPowerMetrics = false;
-			ServerMonitor.Configuration.Config.CollectFanMetrics = false;
-			ServerMonitor.Configuration.Config.CollectServiceMetrics = false;
-			ServerMonitor.Configuration.Config.CollectDockerMetrics = false;
-			ServerMonitor.Configuration.Config.CollectSNMPMetrics = false;
 
 			ServerMonitor.Collector.Collector.HandleCommand( ServerMonitor.Configuration.Config, true );
 
--- Service/ServerMonitor/Source/Configuration.cs
+++ Service/ServerMonitor/Source/Configuration.cs
@@ -61,7 +61,7 @@ namespace ServerMonitor {
 			configurationBuilder.AddJsonFile( GetUserFilePath(), optional: true, reloadOnChange: false );
 
 			// Load non-standard configuration file, if provided
-			if ( extraFilePath != null ) configurationBuilder.AddJsonFile( extraFilePath, optional: true, reloadOnChange: false );
+			configurationBuilder.AddJsonFile( extraFilePath, optional: true, reloadOnChange: false );
 
 			// Load environment variables (e.g., SERVER_MONITOR_TEST=4)
 			configurationBuilder.AddEnvironmentVariables( "SERVER_MONITOR_" );
@@ -77,34 +77,34 @@ namespace ServerMonitor {
 	public sealed class Config {
 
 		// Prometheus metrics server
-		public required string PrometheusListenAddress { get; set; }
-		public required int PrometheusListenPort { get; set; }
-		public required string PrometheusListenPath { get; set; }
-		public required string PrometheusMetricsPrefix { get; set; }
+		public required string PrometheusListenAddress { get; set; } = "127.0.0.1";
+		public required int PrometheusListenPort { get; set; } = 5000;
+		public required string PrometheusListenPath { get; set; } = "metrics/";
+		public required string PrometheusMetricsPrefix { get; set; } = "server_monitor_";
 
 		// Resource metrics
-		public required bool CollectProcessorMetrics { get; set; }
-		public required bool CollectMemoryMetrics { get; set; }
-		public required bool CollectDiskMetrics { get; set; }
-		public required bool CollectNetworkMetrics { get; set; }
-		public required bool CollectUptimeMetrics { get; set; }
-		public required bool CollectPowerMetrics { get; set; }
-		public required bool CollectFanMetrics { get; set; }
+		public required bool CollectProcessorMetrics { get; set; } = true;
+		public required bool CollectMemoryMetrics { get; set; } = true;
+		public required bool CollectDiskMetrics { get; set; } = true;
+		public required bool CollectNetworkMetrics { get; set; } = true;
+		public required bool CollectUptimeMetrics { get; set; } = true;
+		public required bool CollectPowerMetrics { get; set; } = false;
+		public required bool CollectFanMetrics { get; set; } = false;
 
 		// Service metrics
-		public required bool CollectServiceMetrics { get; set; }
+		public required bool CollectServiceMetrics { get; set; } = true;
 
 		// Docker metrics
-		public required bool CollectDockerMetrics { get; set; }
-		public required string DockerEngineAPIAddress { get; set; }
-		public required float DockerEngineAPIVersion { get; set; }
+		public required bool CollectDockerMetrics { get; set; } = true;
+		public required string DockerEngineAPIAddress { get; set; } = "tcp://127.0.0.1:2375";
+		public required float DockerEngineAPIVersion { get; set; } = 1.41f;
 
 		// SNMP options
-		public required bool CollectSNMPMetrics { get; set; }
-		public required string SNMPManagerListenAddress { get; set; }
-		public required int SNMPManagerListenPort { get; set; }
-		public required string SNMPCommunity { get; set; }
-		public required SNMPAgent[] SNMPAgents { get; set; }
+		public required bool CollectSNMPMetrics { get; set; } = true;
+		public required string SNMPManagerListenAddress { get; set; } = "0.0.0.0";
+		public required int SNMPManagerListenPort { get; set; } = 162;
+		public required string SNMPCommunity { get; set; } = "public";
+		public required SNMPAgent[] SNMPAgents { get; set; } = Array.Empty<SNMPAgent>();
 
 	}
 
